68be506dcdaeaa302e1302ab9d7af34bb1f00364
abhinav3254 <abhinav3254@gmail.com>
2023-10-29 19:02:47 +0530

    importing component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
+const Person = () => {
+  return (
+    <>
+      <h1>This is Person Component</h1>
+      <h4>First Name : John</h4>
+      <h4>Last Name : Doe</h4>
+      <p>Age :- {22 + 2}</p>
+    </>
+  );
+}
+
 const App = () => {
+  const name = "abhinav"
   return (
     <div className="App">
-      <h1>Hello Universe</h1>
+      <h1>Hello {name}!</h1>
+      {/* importing Person component here */}
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
 }
