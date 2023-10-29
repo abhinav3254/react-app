d8f40eb7dc34a1d17c8302167ccfa15637d83d2f
abhinav3254 <abhinav3254@gmail.com>
2023-10-29 22:06:49 +0530

    calling the API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+
+const API_URL = 'http://www.omdbapi.com/?apikey=a65ccf09';
+
 
 
 const App = () => {
+    // function which calls the API
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        console.log(data);
+    }
+
+    // Here in this method we called the search movie which will load the data when page is refreshed
+    useEffect(() => {
+        searchMovies(`batman`);
+    }, []);
+
     return (
         <h1>App Js</h1>
     );
