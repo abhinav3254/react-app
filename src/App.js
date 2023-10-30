5ef25d2e1acd5fe0dc1e4ee1d4daf1c9d3da7cab
abhinav3254 <abhinav3254@gmail.com>
2023-10-30 18:24:30 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
+import './App.css';
+import SearchIcon from './search.svg'
+
+import MovieCard from "./MovieCard";
+
 
 const API_URL = 'http://www.omdbapi.com/?apikey=a65ccf09';
 
-
+const movie1 = {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
 
 const App = () => {
+
+    const [movies, setMovies] = useState([]);
+
     // function which calls the API
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json();
-        console.log(data);
+        setMovies(data.Search);
     }
 
     // Here in this method we called the search movie which will load the data when page is refreshed
@@ -19,7 +33,36 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App Js</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input
+                    placeholder="search for movies"
+                    value="superman"
+                    onChange={() => { }}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => { }}
+                />
+            </div>
+
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            <MovieCard movie1={movies[5]} />
+                        </div>
+                    ) :
+                    (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
+
+        </div>
     );
 }
 
